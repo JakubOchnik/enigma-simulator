ab2725469192383df7b89cd69c5e31d29105919b
JakubOchnik <jakub.ochnik1@gmail.com>
2020-07-13 11:45:54 +0200

    Add text commands to make data input user-friendly

--- enigma.c
+++ enigma.c
@@ -10,7 +10,11 @@ int main() {
 	int alphSize, rotorCount, reflCount; // size of alphabet, number of rotors, number of reflectors
 	int** rotorNotchDef; // notches of each rotor
 	int** reflDef; // definitions of reflectors
+	printf("SIMPLE ENIGMA SIMULATOR\nAuthor: Jakub Ochnik\n\n");
+	printf("WARNING! Please note that in this program letters are only represented by numbers.\n Regular (a-z) letters are not supported yet.\n\n");
+	printf("Enter the length of the numerical alphabet:\n");
 	scanf("%d", &alphSize);
+	printf("Enter the number of rotors:\n");
 	scanf("%d", &rotorCount);
 
 	rotorNotchDef = (int**)malloc(rotorCount * sizeof(int*));
@@ -25,6 +29,7 @@ int main() {
 		(rotor + i)->backAlph = (int*)malloc(alphSize * sizeof(int));
 	}
 	for (int i = 0; i < rotorCount; i++) {
+		printf("\t --ROTOR %d--\nSpecify the initial position of rotor (letters separated by spaces):\n",i);
 		for (int j = 0; j < alphSize; j++)
 		{
 			int rotorLetter;
@@ -46,21 +51,29 @@ int main() {
 				}
 			}
 		}
+		printf("Enter the number of turnover letters (notches):\n");
 		scanf("%d", &(rotorNotchesCount[i]));
 		rotorNotchDef[i] = (int*)malloc(rotorNotchesCount[i] * sizeof(int));
+		if(rotorNotchesCount[i]>0)
+			printf("Enter turnover letters (separated by spaces):\n");
 		for (int j = 0; j < rotorNotchesCount[i]; j++)
 			scanf("%d", &(rotorNotchDef[i][j]));
 	}
+	printf("Enter the number of reflectors:\n");
 	scanf("%d", &reflCount);
 	reflDef = (int**)malloc(reflCount * sizeof(int));
 	for (int i = 0; i < reflCount; i++)
 		reflDef[i] = (int*)malloc(alphSize * sizeof(int));
 
-	for (int i = 0; i < reflCount; i++)
-		for (int j = 0; j < alphSize; j++)
+	for (int i = 0; i < reflCount; i++) {
+		printf("Enter the definition of reflector %d (letters separated by whitespaces):\n", i);
+		for (int j = 0; j < alphSize; j++) {
 			scanf("%d", &(reflDef[i][j]));
+		}
+	}
 
 	int problemCount;
+	printf("Enter the number of messages to encrypt:\n");
 	scanf("%d", &problemCount);
 	problem* params = (problem*)malloc(sizeof(problem));
 	for (int i = 0; i < problemCount; i++) {
@@ -70,21 +83,30 @@ int main() {
 	int currentNumber;
 	int index;
 	for (int i = 0; i < problemCount; i++) {
+		printf("\t--MESSAGE %d--\n",i);
 		int z = 0;
+		printf("Enter the number of rotors to use:\n");
 		scanf("%d", &(params->rotCount));
 		params->rotInd = (int*)malloc(params->rotCount * sizeof(int));
 		params->initPos = (int*)malloc(params->rotCount * sizeof(int));
 		while (z < params->rotCount * 2) {
-			if (z % 2 == 0)
+			if (z % 2 == 0) {
+				printf("Enter the index of rotor %d:\n",z/2);
 				scanf("%d", &(params->rotInd[z / 2]));
-			else
+			}
+			else {
+				printf("Enter the initial position of rotor %d:\n", z/2);
 				scanf("%d", &(params->initPos[z / 2]));
+			}
 			z++;
 		}
+		printf("Enter the index of reflector:\n");
 		scanf("%d", &(params->reflInd));
 		params->num = (int*)malloc(10 * sizeof(int));
+		printf("Enter a sequence of numbers to encrypt:\n");
 		params->len = getAllocLineInput(params);
 
+		printf("ENCODED MESSAGE %d:\n",i);
 		// START OF ENCODING
 		int* rotorStart = (int*)malloc(params->rotCount * sizeof(int));
 		int rotorSteps[3] = { 0 };
